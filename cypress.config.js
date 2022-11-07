4aa4f695931832fe26d9dcaab5231be00ffd9c26
eby-shop <freddy@join-eby.com>
2022-11-07 14:41:48 -0500

    Added bikines, bralette packs for desktop

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
-    viewportWidth: 375,
-    viewportHeight: 667,
-    baseUrl: 'https://shop.join-eby.com/',
+    // viewportWidth: 375,
+    // viewportHeight: 667,
+    viewportWidth: 1024,
+    viewportHeight: 640,
+    baseUrl: 'https://ua4zxmhry56sxqf4-31340625964.shopifypreview.com/',
 
   },
 });
---
+++ cypress/e2e/packs/bralettes-desktop.cy.js
@@ -0,0 +1,54 @@
+const getIframeDocument = () => {
+  return cy
+  .get('iframe#attentive_creative')
+  .its('0.contentDocument').should('exist');
+}
+
+const getIframeBody = () => {
+  // get the document
+  return getIframeDocument()
+  .its('body').should('not.be.undefined')
+  .then(cy.wrap);
+}
+describe('Exit iframe', () => {
+  it('exit from iframe if any',() => {
+    cy.visit('/products/laurel-green-and-blue-opal-cotton-bralettes-pack');
+      getIframeBody().find('#closeIconContainer').should('have.class', 'css-upw05v').click();
+  });
+});
+
+describe('1. Bralettes', () => {
+
+  it(`Adds 1 item to cart`, () => {
+    cy.get('#AddToCart').click();
+    cy.wait(5000);
+  }); 
+   it(`Adds 2 item to cart`, () => {
+    cy.get('#AddToCart').click();
+  }); 
+  
+
+  const cartItem = 'form.cart.ajaxcart .drawerProduct.ajaxCartRow';
+  const itemTitle = ' .onetimeTitle.prod_line';
+  const price = '.drawerActionsFooter .drawerProductPrice .bfx-product-subtotal.bfx-price';
+  const listItem = '.cartItemsListingWrapper .ajaxCartProduct.bfx-product > .drawerProduct.ajaxCartRow';
+  const prop = 'data-prop';
+  const dataVid = 'data-vid';
+
+
+  it(`Has item in cart with price $160`, () => {
+    cy.get(price).then(list => {
+      const newText = Cypress.$(list[0]).text()
+
+      expect(newText).to.equal('$160');
+    })
+  });
+});
+
+
+
+
+
+
+
+
