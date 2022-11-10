55584d2e339a9cb654be8cbecf2069860c3cbb12
eby-shop <freddy@join-eby.com>
2022-11-10 15:22:14 -0500

    packs parent and children

--- cypress/e2e/quickAdd/braletees/index.cy.js
+++ cypress/e2e/quickAdd/braletees/index.cy.js
@@ -19,27 +19,63 @@ describe('Exit iframe', () => {
   });
 });
 
+const bralettes = {
+  parent: ['6974099161132', '6974099390508'],
+  green: [6793270558764, 6773865119788],
+  strawberry: [6758604996652,6793245720620]
+}
+
 describe('1. Bralettes', () => {
 
- it('hovers on 1st item', { scrollBehavior: false },() => {
-    cy.get('.productQuickAdd').then(list => {
-      cy.wrap(list[0]).trigger('mouseover')
-      cy.wait(3000);
+
+  let braletteColor = 'green'
+  it(`Green 6974099161132 has data-product ${bralettes[braletteColor][0]} and ${bralettes[braletteColor][1]}`, { scrollBehavior: false },() => {
+    let i = 0
+    cy.get(`.proButton.mb20.dan-6974099161132 .single-product`).then(pdp =>{
+      while(i < 2){
+        cy.wrap(pdp[i]).invoke('attr','data-product')
+          .should('eq', `${bralettes[braletteColor][i]}`)
+        i++;
+      }
     });
-  });
- it('clicks on lg and send to cart',{ scrollBehavior: false },() => {
-    cy.get('.proVariantsQuickview .swatch-element.lg[data-value="lg"]').click()
   });
 
-  it('hovers on 2nd item',{ scrollBehavior: false },() => {
-    cy.get('.productQuickAdd').then(list => {
-      cy.wrap(list[1]).trigger('mouseover')
-      cy.wait(3000);
+  const bColor = 'strawberry' 
+  it(`Strawberry 6974099390508 has data-product ${bralettes[bColor][0]} and ${bralettes[bColor][1]}`, { scrollBehavior: false },() => {
+    let i = 0
+    cy.get(`.proButton.mb20.dan-6974099390508 .single-product`).then(pdp =>{
+      while(i < 2){
+        cy.wrap(pdp[i]).invoke('attr','data-product')
+          .should('eq', `${bralettes[bColor][i]}`)
+        i++;
+      }
     });
   });
- it('clicks on md and send to cart',{ scrollBehavior: false },() => {
-    cy.get('.swatch-element.md[data-value="md"]').click()
-  });
+
+  // ADD TO CART
+  // it('hovers on 1st item', { scrollBehavior: false },() => {
+  //   cy.get('.productQuickAdd').then(list => {
+  //     cy.wrap(list[0]).trigger('mouseover')
+  //     cy.wait(3000);
+  //   });
+  // });
+  // it('clicks on lg and send to cart',{ scrollBehavior: false },() => {
+  //   cy.get('.proVariantsQuickview .swatch-element.lg[data-value="lg"]').click()
+  // });
+
+
+
+
+
+ //  it('hovers on 2nd item',{ scrollBehavior: false },() => {
+ //    cy.get('.productQuickAdd').then(list => {
+ //      cy.wrap(list[1]).trigger('mouseover')
+ //      cy.wait(3000);
+ //    });
+ //  });
+ // it('clicks on md and send to cart',{ scrollBehavior: false },() => {
+ //    cy.get('.swatch-element.md[data-value="md"]').click()
+ //  });
 });
 
 
--- cypress/e2e/quickAdd/panties/index.cy.js
+++ cypress/e2e/quickAdd/panties/index.cy.js
@@ -14,13 +14,92 @@ const URL = '/collections/seamless-panties-packs?preview_theme_id=126410522668'
 describe('Exit iframe', () => {
   it('exit from iframe if any',() => {
     cy.visit(URL);
-    cy.wait(5000);
+    cy.wait(3000);
       getIframeBody().find('#closeIconContainer').should('have.class', 'css-upw05v').click();
       cy.scrollTo(0, 300);
   });
 });
 
-describe('1. Panties', () => {
+const meshHighCut = {
+  parent: '6984109686828',
+  children: [
+    {'6599022247980':'productSelector-black-mesh-highwaisted-panties'},
+    {'6599107313708':'productSelector-coral-pink-mesh-high-waisted-panties'},
+    {'6599095910444':'productSelector-caribbean-sea-mesh-highwaisted-panties'}
+  ],
+  keys: ['6599022247980','6599107313708', '6599095910444']
+}
+
+describe('1. Mesh Thong Pack', () => {
+
+
+  it(`1. ${meshHighCut['parent']} has data-product 6599022247980, 6599107313708, 6599095910444`, { scrollBehavior: false },() => {
+    let i = 0
+    cy.get(`.proButton.mb20.dan-6984109686828 .single-product`).then(pdp =>{
+      while(i < 2){
+        cy.wrap(pdp[i]).invoke('attr','data-product')
+          .should('eq', `${meshHighCut['keys'][i]}`)
+        i++;
+      }
+    });
+  });
+
+});
+
+
+
+const meshThong = {
+  parent: '6984110145580',
+  children: [
+    {'6599021101100':'productSelector-black-mesh-thong-panties'},
+    {'6599097778220':'productSelector-caribbean-sea-mesh-thong-panties'},
+    {'6599108558892':'productSelector-coral-pink-mesh-thong-panties'}
+  ],
+  keys: ['6599021101100','6599097778220', '6599108558892']
+}
+
+describe('1. Mesh Thong Pack', () => {
+
+
+  it(`2. ${meshThong['parent']} has data-product ${meshThong.keys[0]}, ${meshThong.keys[1]}, ${meshThong.keys[2]}`, { scrollBehavior: false },() => {
+    let i = 0
+    cy.get(`.proButton.mb20.dan-6984110145580 .single-product`).then(pdp =>{
+      while(i < 2){
+        cy.wrap(pdp[i]).invoke('attr','data-product')
+          .should('eq', `${meshThong['keys'][i]}`)
+        i++;
+      }
+    });
+  });
+});
+
+
+const meshBreif = {
+  parent: '6984108900396',
+  children: [
+    {'6599017005100':'productSelector-black-mesh-brief-panties'},
+    {'6599105839148':'productSelector-coral-pink-mesh-brief-panties'},
+    {'6599094173740':'productSelector-caribbean-sea-mesh-brief-panties'}
+  ],
+  keys: ['6599017005100','6599105839148', '6599094173740'],
+  selector: '.proButton.mb20.dan-6984108900396 .single-product'
+}
+
+describe('1. Mesh Thong Pack', () => {
+
+
+  it(`2. ${meshBreif['parent']} has data-product ${meshBreif.keys[0]}, ${meshBreif.keys[1]}, ${meshBreif.keys[2]}`, { scrollBehavior: false },() => {
+    let i = 0
+    cy.get(meshBreif.selector).then(pdp =>{
+      while(i < 2){
+        cy.wrap(pdp[i]).invoke('attr','data-product')
+          .should('eq', `${meshBreif['keys'][i]}`)
+        i++;
+      }
+    });
+  });
+});
+/*
 
  it('hovers on 1st item',{ scrollBehavior: false },() => {
     cy.get('.productQuickAdd').then(list => {
@@ -39,9 +118,6 @@ describe('1. Panties', () => {
   it('clicks on md and send to cart',{ scrollBehavior: false },() => {
     cy.get('.swatch-element.md[data-value="md"]').click()
   });
-
-
-
 
 
   it('hovers on 4nd item',() => {
@@ -104,8 +180,7 @@ describe('1. Panties', () => {
   it('clicks on md and send to cart',{ scrollBehavior: false },() => {
     cy.get('.swatch-element.md[data-value="md"]').click()
   });
-
-});
+*/
 
 
 
