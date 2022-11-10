d29e1044062d9570519121d6a7371b3eea55bf22
eby-shop <freddy@join-eby.com>
2022-11-10 09:44:20 -0500

    panties pack

--- cypress/e2e/quickAdd/panties/index.cy.js
+++ cypress/e2e/quickAdd/panties/index.cy.js
@@ -14,6 +14,7 @@ const URL = '/collections/seamless-panties-packs?preview_theme_id=126410522668'
 describe('Exit iframe', () => {
   it('exit from iframe if any',() => {
     cy.visit(URL);
+    cy.wait(5000);
       getIframeBody().find('#closeIconContainer').should('have.class', 'css-upw05v').click();
       cy.scrollTo(0, 300);
   });
@@ -66,41 +67,41 @@ describe('1. Panties', () => {
 
 
   it('hovers on Mystery Pack',() => {
-    cy.scrollTo(0, 1200);
+    cy.scrollTo(0, 1800);
     cy.get('.productQuickAdd').then(list => {
       
       cy.wrap(list[6]).trigger('mouseover')
     });
   });
-  it('clicks on md and send to cart',() => {
+  it('clicks on md and send to cart',{ scrollBehavior: false },() => {
     cy.get('.swatch-element.md[data-value="md"]').click()
   });
-  it('hovers on Everyday Seamless 3 Panties Pack',() => {
+  it('hovers on Everyday Seamless 3 Panties Pack',{ scrollBehavior: false },() => {
     cy.get('.productQuickAdd').then(list => {
       cy.wrap(list[7]).trigger('mouseover')
     });
   });
-  it('clicks on md and send to cart',() => {
+  it('clicks on md and send to cart',{ scrollBehavior: false },() => {
     cy.get('.swatch-element.md[data-value="md"]').click()
   });
 
 
 
    it('hovers on Spanish Rivera',() => {
-     cy.scrollTo(0, 1600);
+     cy.scrollTo(0, 2600);
     cy.get('.productQuickAdd').then(list => {
       cy.wrap(list[8]).trigger('mouseover')
     });
   });
-  it('clicks on md and send to cart',() => {
+  it('clicks on md and send to cart',{ scrollBehavior: false },() => {
     cy.get('.swatch-element.md[data-value="md"]').click()
   });
-  it('hovers on Desert Thong Pack',() => {
+  it('hovers on Desert Thong Pack',{ scrollBehavior: false },() => {
     cy.get('.productQuickAdd').then(list => {
       cy.wrap(list[9]).trigger('mouseover')
     });
   });
-  it('clicks on md and send to cart',() => {
+  it('clicks on md and send to cart',{ scrollBehavior: false },() => {
     cy.get('.swatch-element.md[data-value="md"]').click()
   });
 
